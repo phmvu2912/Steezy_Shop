58e0fcc5d1ec55ce03eefa59c999821434ee2f68
phmvu2912 <phmvu2912@gmail.com>
2024-07-20 01:19:53 +0700

    config axios

--- fe/src/configs/axios.ts
+++ fe/src/configs/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:5173/",
+    baseURL: "http://localhost:8000/api/v1",
     timeout: 3000,
     headers: {
         "Content-Type": "application/json"
